f1f708bbcdf739f5dc4a38f72c88528606dc46dd
yash10019coder <lit2020066@iiitl.ac.in>
2021-04-17 16:22:06 +0530

    imported some libraries in project

--- app/src/main/java/com/yash10019coder/videocalling/MainActivity.java
+++ app/src/main/java/com/yash10019coder/videocalling/MainActivity.java
@@ -3,7 +3,10 @@ package com.yash10019coder.videocalling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.yash10019coder.videocalling.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityMainBinding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
+        activityMainBinding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Log.i("TAG", "onClick: "+FirebaseAuth.getInstance().getCurrentUser());
+
+            }
+        });
     }
 }
